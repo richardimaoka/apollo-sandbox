f3b05d69861e71d5e945259f04488bc6b5ec99ba
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-06-05 01:37:53 +0900

    custom scalar mock

--- index.js
+++ index.js
@@ -12,11 +12,14 @@ const typeDefs = gql`
     author: String
   }
 
+  scalar MyCustomScalar
+
   # The "Query" type is special: it lists all of the available queries that
   # clients can execute, along with the return type for each. In this
   # case, the "books" query returns an array of zero or more Books (defined above).
   type Query {
     books: [Book]
+    scalar: MyCustomScalar
   }
 `;
 
