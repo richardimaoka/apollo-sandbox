e69d27b0604daf09b0de710528b0f81bcf171634
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-06-05 01:44:29 +0900

    not working perfectly

--- index.js
+++ index.js
@@ -1,44 +1,54 @@
 const { ApolloServer, gql } = require("apollo-server");
+const { GraphQLScalarType, Kind } = require("graphql");
+
+const dateScalar = new GraphQLScalarType({
+  name: "Date",
+  description: "Date custom scalar type",
+  serialize(value) {
+    return value.getTime(); // Convert outgoing Date to integer for JSON
+  },
+  parseValue(value) {
+    return new Date(value); // Convert incoming integer to Date
+  },
+  parseLiteral(ast) {
+    if (ast.kind === Kind.INT) {
+      return new Date(parseInt(ast.value, 10)); // Convert hard-coded AST string to integer and then to Date
+    }
+    return null; // Invalid hard-coded value (not an integer)
+  },
+});
 
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
 // your data.
 const typeDefs = gql`
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+  scalar Date
 
-  # This "Book" type defines the queryable fields for every book in our data source.
-  type Book {
-    title: String
-    author: String
+  type Event {
+    id: ID!
+    date: Date!
   }
 
-  scalar MyCustomScalar
-
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
   type Query {
-    books: [Book]
-    scalar: MyCustomScalar
+    events: [Event!]
   }
 `;
 
-const books = [
+const events = [
   {
-    title: "The Awakening",
-    author: "Kate Chopin",
+    id: "The Awakening",
   },
   {
-    title: "City of Glass",
-    author: "Paul Auster",
+    id: "The Awakening",
   },
 ];
 
 // Resolvers define the technique for fetching the types defined in the
 // schema. This resolver retrieves books from the "books" array above.
 const resolvers = {
+  Date: dateScalar,
   Query: {
-    books: () => books,
+    events: () => events,
   },
 };
 
