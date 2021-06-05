a6d20d325bd9ef62e368ec4f1d40ec877b3fdc80
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-06-06 04:13:29 +0900

    odd

--- index.js
+++ index.js
@@ -1,98 +1,37 @@
 const { ApolloServer, gql } = require("apollo-server");
 const { GraphQLScalarType, Kind } = require("graphql");
 
-const dateScalar = new GraphQLScalarType({
-  name: "Date",
-  description: "Date custom scalar type",
-  serialize(value) {
-    return value.getTime(); // Convert outgoing Date to integer for JSON
-  },
-  parseValue(value) {
-    return new Date(value); // Convert incoming integer to Date
-  },
-  parseLiteral(ast) {
-    if (ast.kind === Kind.INT) {
-      return new Date(parseInt(ast.value, 10)); // Convert hard-coded AST string to integer and then to Date
-    }
-    return null; // Invalid hard-coded value (not an integer)
-  },
-});
-
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
+// Basic schema
 const typeDefs = gql`
-  scalar Date
+  scalar Odd
 
-  type SomeObj {
-    i: Int
-    s: String
-  }
-
-  type Event {
-    id: ID!
-    date: Date
-    someObj: SomeObj
-  }
-
-  type Query {
-    events: [Event!]
+  type MyType {
+    oddValue: Odd
   }
 `;
 
-const events = [
-  {
-    id: "The Awakening",
-  },
-  {
-    id: "The Awakening",
-  },
-];
+// Validation function
+function oddValue(value) {
+  return value % 2 === 1 ? value : null;
+}
 
-// Resolvers define the technique for fetching the types defined in the
-// schema. This resolver retrieves books from the "books" array above.
 const resolvers = {
-  //SomeObj: {
-  //  i: () => 1,
-  //  s: () => "str",
-  //},
-  Event: {
-    someObj(parent, args, context, info) {
-      console.log("----------------------------");
-      console.log(parent);
-      console.log(context);
-      return {
-        i: 1,
-        s: "str",
-      };
+  Odd: new GraphQLScalarType({
+    name: "Odd",
+    description: "Odd custom scalar type",
+    parseValue: oddValue,
+    serialize: oddValue,
+    parseLiteral(ast) {
+      if (ast.kind === Kind.INT) {
+        return oddValue(parseInt(ast.value, 10));
+      }
+      return null;
     },
-  },
-  Date: dateScalar,
-  Query: {
-    events: () => events,
-  },
+  }),
 };
 
-// The ApolloServer constructor requires two parameters: your schema
-// definition and your set of resolvers.
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: function (r) {
-    //console.log(r.req.complete);
-    //console.log(r.req.rawHeaders);
-    //console.log(r.req.url);
-    //console.log(r.req.method);
-    //console.log(r.req.statusCode);
-    //console.log(r.req.params);
-    //console.log(r.req.query);
-    //console.log(r.req.body);
-    console.log(r.req.body.operationName);
-    return { a: "aaa", b: "bbb" };
-  },
-});
+const server = new ApolloServer({ typeDefs, resolvers });
 
-// The `listen` method launches a web server.
 server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
+  console.log(`🚀 Server ready at ${url}`);
 });
