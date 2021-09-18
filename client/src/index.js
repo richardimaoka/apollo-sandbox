3beae90081d27459315244f97016526ffec6eafa
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-09-18 23:27:43 +0900

    use local

--- client/src/index.js
+++ client/src/index.js
@@ -10,7 +10,7 @@ import {
 } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "https://sxewr.sse.codesandbox.io/",
+  uri: "http://localhost:4000",
   cache: new InMemoryCache(),
 });
 
