b75afdb6ae4d6c4a3fb154078dd492da783cd30d
Richard Imaoka <richard.e.imaoka@gmail.com>
2022-06-20 23:48:04 +0900

    display fields

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,12 +8,31 @@ import {
 } from "@apollo/client";
 import "./App.css";
 
+interface QueryData {
+  feed: {
+    id: string;
+    name: string;
+    age: number;
+  }[];
+}
+
 export const Internal = (): JSX.Element => {
-  const { loading, error, data } = useQuery(gql`
+  const { loading, error, data } = useQuery<QueryData>(gql`
     query ExampleQuery {
-      feed {
+      feed(offset: 40, limit: 20) {
         id
         name
+        age
+        age
+      }
+    }
+  `);
+  const {} = useQuery(gql`
+    query ExampleQuery {
+      feed(offset: 20, limit: 20) {
+        id
+        name
+        age
         age
       }
     }
@@ -32,14 +51,41 @@ export const Internal = (): JSX.Element => {
   } else {
     console.log("success", data);
     console.log(client.cache);
-    return <div>success</div>;
+    return (
+      <div>
+        {data.feed.map((n) => (
+          <div key={n.id}>
+            <div>{n.id}</div>
+            <div>{n.name}</div>
+            <div>{n.age}</div>
+            <div>========</div>
+          </div>
+        ))}
+      </div>
+    );
   }
 };
 
 function App() {
   const client = new ApolloClient({
     uri: "http://localhost:4000/",
-    cache: new InMemoryCache(),
+    cache: new InMemoryCache({
+      typePolicies: {
+        Query: {
+          fields: {
+            feed: {
+              // Don't cache separate results based on
+              // any of this field's arguments.
+              keyArgs: ["limit"],
+              read(existing) {
+                console.log("read function is called", existing);
+                return existing;
+              },
+            },
+          },
+        },
+      },
+    }),
   });
 
   return (
