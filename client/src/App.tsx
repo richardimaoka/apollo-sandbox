6ca5b409b76c27a4fbe97f9963c98cc9e1919078
Richard Imaoka <richard.e.imaoka@gmail.com>
2022-06-20 22:00:01 +0900

    cache analysis

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  ApolloClient,
+  ApolloProvider,
+  gql,
+  InMemoryCache,
+  useApolloClient,
+  useQuery,
+} from "@apollo/client";
+import "./App.css";
+
+export const Internal = (): JSX.Element => {
+  const { loading, error, data } = useQuery(gql`
+    query ExampleQuery {
+      feed {
+        id
+        name
+        age
+      }
+    }
+  `);
+  const client = useApolloClient();
+
+  if (loading) {
+    console.log("loading");
+    return <div>loading</div>;
+  } else if (error) {
+    console.log("error", error);
+    return <div>error</div>;
+  } else if (!data) {
+    console.log("no data");
+    return <></>;
+  } else {
+    console.log("success", data);
+    console.log(client.cache);
+    return <div>success</div>;
+  }
+};
 
 function App() {
+  const client = new ApolloClient({
+    uri: "http://localhost:4000/",
+    cache: new InMemoryCache(),
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <Internal />
+    </ApolloProvider>
   );
 }
 
